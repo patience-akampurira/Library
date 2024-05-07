494d48e72afc7b302831e575ce32e17a5ae949dd
patience-akampurira <patienceakampurira79@gmail.com>
2024-05-07 12:52:25 +0300

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12" style="text-align:center">
+                   &copy; 2024 Library Management System |<a href="#" target="_blank"  > Designed by : Panash</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
